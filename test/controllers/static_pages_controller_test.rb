0cc23b26eb7fc0a5b54291e0414d4e41ac81544a
noriko <noriko@norikonoMacBook-Air.local>
2020-07-11 01:43:58 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,21 +5,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Instagram"
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     assert_select "title", @base_title
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get help" do
+    get help_url
+    assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
 
 end
