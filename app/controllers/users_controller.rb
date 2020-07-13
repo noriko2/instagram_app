40a84a7d45221cfae1d0d5b381bf4d324fdbe32c
noriko <noriko@norikonoMacBook-Air.local>
2020-07-13 15:44:36 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    #@user = User.new(params[:user]) ←安全性× Forbidden Attribute Errorが出る
+    if @user.save
+      flash[:success] = "インスタグラムへようこそ！"
+      redirect_to @user
+      #上記は、redirect_to user_url(@user.id)と同じ
+      # => showアクションへ /users/:id
+    else
+      render 'new'
+    end
+  end
+
+  private
+    #ストロングパラメーターを定義(安全性のため、入力内容の制限)
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  
+
   root 'static_pages#home'
   get '/about', to: 'static_pages#about'
   get '/help',  to: 'static_pages#help'
 
   get '/signup', to: 'users#new'
+
+  resources :users
 
 end
