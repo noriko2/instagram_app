9c7b4910f1543833e976cf9454d155e09518b3c0
noriko <noriko@norikonoMacBook-Air.local>
2020-09-04 01:50:37 +0900

    herokuアドレスの変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'instagram--app.herokuapp.com'
+  host = 'noristagram-1.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
